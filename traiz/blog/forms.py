bf762bb51af734552cca3c8406445f3b33aa40a1
SanniClara <33022007+SanniClara@users.noreply.github.com>
2020-12-23 18:40:51 +0100

    push no. 2
    added date Form

--- traiz/blog/forms.py
+++ traiz/blog/forms.py
@@ -1,11 +1,33 @@
 from django import forms
 
 class NameForm(forms.Form):
-    country = forms.CharField(label='Country:', max_length=100)
-    company = forms.CharField(label='Company:', max_length=100)
-    company = forms.CharField(label='Company:', max_length=100)
+    country = forms.CharField(label='Country:', max_length=100, required=False)
+    company = forms.CharField(label='Company:', max_length=100, required=False)
+    industry = forms.CharField(label='Industry:', max_length=100, required=False)
+    segment_Spezialication = forms.CharField(label='Segment/Spezialication:', max_length=100, required=False)
+    main_Market = forms.CharField(label='Main Market:', max_length=100, required=False)
+    keyword_Search = forms.CharField( widget=forms.Textarea(attrs={"rows":5, "cols":20}), label='keyword_Search:', max_length=100, required=False)
 
 
+class Proritize_Indicato(forms.Form):
+    alliances = forms.BooleanField(label='Alliances', required=False )
+    alternativeEngines = forms.BooleanField(label='Alternative Engines', required=False )
+    capacityIncrease = forms.BooleanField(label='capacity Increase', required=False )
+    companyLaunch = forms.BooleanField(label='Company Launch', required=False )
+    corporateFinance = forms.BooleanField(label='Corporate Finance', required=False )
+    corporateMA = forms.BooleanField(label='Corporate M&A', required=False )
+    humanResource = forms.BooleanField(label='Human Resource', required=False )
+    presicionTechnology = forms.BooleanField(label='Presicion Technology', required=False )
+    innovation = forms.BooleanField(label='Innovation', required=False )
+    productLaunches = forms.BooleanField(label='Product Launches', required=False )
+    productUpgrades = forms.BooleanField(label='Product Upgrades', required=False )
+    reporting = forms.BooleanField(label='Reporting', required=False)
+    strategy = forms.BooleanField(label='Strategy', required=False)
 
+class DateInput(forms.DateInput):
+      input_type = 'date'
 
-    
+class DateForm(forms.Form):
+      date = forms.DateField(label='From ', required= False, widget=DateInput)
+      date2 = forms.DateField(label='To ', required= False, widget=DateInput)
+
--- traiz/blog/views.py
+++ traiz/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import NameForm
-
+from .forms import NameForm, Proritize_Indicato, DateForm
+import csv
 
 # Create your views here.
 data_generated=[
@@ -32,7 +32,43 @@ data_generated=[
 ]
 
 def dash_board(request):
-    return render(request, 'blog/home.html')
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST': 
+        form = NameForm(request.POST)
+        date = DateForm(request.POST)
+
+        if form.is_valid():
+            country = form.cleaned_data['country']
+            company = form.cleaned_data['company']
+            industry = form.cleaned_data['industry']
+            segment_Spezialication = form.cleaned_data['segment_Spezialication']
+            main_Market = form.cleaned_data['main_Market']
+            keyword_Search = form.cleaned_data['keyword_Search']
+            print (request.POST.getlist('date'))
+            print("Company: " + company + '_cut_') 
+            print("industry: " + industry + '_cut_')    
+            print("segment: " + segment_Spezialication + '_cut_')   
+            print("main Market: " + main_Market + '_cut_')    
+            print("Keywords: " + keyword_Search + '_cut_') 
+            #print(request.POST)
+            #print( 'alliance:' + str(request.POST.getlist('alliances')))
+            #if request.POST.getlist('alliances'): 
+            #  varialeAlliance = 'alliance'
+            #  print('Indicator:' + varialeAlliance + '_cut_')
+
+    else:
+         form = NameForm()
+         date = DateForm()
+
+    return render(request, 'blog/home.html', {
+        'form': form, 
+        'date' : date
+    })
+
+    if date.is_valid():
+        date = request.POST.getlist('date')
+        date = date.cleaned_data()
+        print(date)
 
 def about(request):
     return render(request, 'blog/about.html')
@@ -52,30 +88,87 @@ def notification(request):
     }
     return render(request, 'blog/notification.html', context)
 
-def results(request):
-    return render(request, 'blog/results.html')
+def results(request):        # if this is a POST request we need to process the form data
+        # if this is a POST request we need to process the form data
+    if request.method == 'POST': 
+        form = NameForm(request.POST)
+        date = DateForm(request.POST)
+
+        if form.is_valid():
+            country = form.cleaned_data['country']
+            company = form.cleaned_data['company']
+            industry = form.cleaned_data['industry']
+            segment_Spezialication = form.cleaned_data['segment_Spezialication']
+            main_Market = form.cleaned_data['main_Market']
+            keyword_Search = form.cleaned_data['keyword_Search']
+            print (request.POST.getlist('date'))
+            print("Company: " + company + '_cut_') 
+            print("industry: " + industry + '_cut_')    
+            print("segment: " + segment_Spezialication + '_cut_')   
+            print("main Market: " + main_Market + '_cut_')    
+            print("Keywords: " + keyword_Search + '_cut_') 
+            #print(request.POST)
+            #print( 'alliance:' + str(request.POST.getlist('alliances')))
+            #if request.POST.getlist('alliances'): 
+            #  varialeAlliance = 'alliance'
+            #  print('Indicator:' + varialeAlliance + '_cut_')
+
+    else:
+         form = NameForm()
+         date = DateForm()
+
+    return render(request, 'blog/results.html', {
+        'form': form, 
+        'date' : date
+    })
+
+    if date.is_valid():
+        date = request.POST.getlist('date')
+        date = date.cleaned_data()
+        print(date)
 
 def target_finder(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST': 
         form = NameForm(request.POST)
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+        char_1 = Proritize_Indicato(request.POST)
+        date = DateForm(request.POST)
+        
+
+        if form.is_valid() or char_1.is_valid():
             country = form.cleaned_data['country']
             company = form.cleaned_data['company']
+            industry = form.cleaned_data['industry']
+            segment_Spezialication = form.cleaned_data['segment_Spezialication']
+            main_Market = form.cleaned_data['main_Market']
+            keyword_Search = form.cleaned_data['keyword_Search']
+            charrty = char_1['alliances']
+            print("Company: " + company + '_cut_') 
+            print("industry: " + industry + '_cut_')    
+            print("segment: " + segment_Spezialication + '_cut_')   
+            print("main Market: " + main_Market + '_cut_')    
+            print("Keywords: " + keyword_Search + '_cut_') 
+            #print(request.POST)
+            #print( 'alliance:' + str(request.POST.getlist('alliances')))
+            if request.POST.getlist('alliances'): 
+                varialeAlliance = 'alliance'
+                print('Indicator:' + varialeAlliance + '_cut_')
+            return render(request, 'blog/results.html', {
+                    'form': form, 
+                    'date' : date
+                })
 
-            print("1")        
-            print("country: " + country)    
-            print("company: " + company)    
-
-            return render(request, 'blog/results.html')
     # if a GET (or any other method) we'll create a blank form
     else:
          form = NameForm()
-        
+         char_1 = Proritize_Indicato()
+         date = DateForm()
+
   
-    return render(request, 'blog/target_finder.html', {'form': form})
+    return render(request, 'blog/target_finder.html', {
+        'form': form,
+        'char_1': char_1})
+
+
 
 
